6cb3730a965379e448b3c36436c71351bb705ce3
petrit123 <petrit.krasniqi@students.ittralee.ie>
2020-05-09 13:58:12 +0100

    Final commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -24,16 +24,16 @@
                 <v-spacer></v-spacer>
               </v-toolbar>
               <v-card-text>
-                <v-form v-model="isValid" color="green" v-on:submit="validateLogin">
+                <v-form v-model="isValid" color="green">
                   <v-text-field
-                              v-model="first"
+                              v-model="input.username"
             :rules="[rules.required, rules.min]"
             label="Username"
             
                   ></v-text-field>
 
                   <v-text-field
-                              v-model="password"
+                              v-model="input.password"
             :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
             :rules="[rules.required, rules.min]"
             :type="show1 ? 'text' : 'password'"
@@ -46,32 +46,61 @@
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="primary" :disabled="!isValid">Login</v-btn>
+                <v-btn v-on:click="validateLogin" color="primary" :disabled="!isValid">Login</v-btn>
+                 <br>
               </v-card-actions>
             </v-card>
+                                <span align="center">
+     <b> {{ apiResponse }} </b>
+  </span>
+  <br>
           </v-col>
         </v-row>
       </v-container>
+      
     </v-content>
+    
   </v-app>
 </template>
 
 <script>
+
+
+
   export default {
     data () {
       return {
+        input: {},
         show1: false,
         password: '',
         isValid: true,
+        apiResponse: null,
         rules: {
           required: value => !!value || 'Required.',
         },
       }
     },
     methods: {
-
+          
             async validateLogin() {
+              console.log(this.input.username);
+              console.log(this.input.password);
+              let apiResponse = '';
+
+                await fetch("http://localhost:5094/user/login", {
+                method: 'POST',
+                headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                  username: this.input.username, password: this.input.password
+                })
+              }).then(res => res.text())
+              .then(text => apiResponse = text)
+
+              this.apiResponse = apiResponse;
+
+              if (apiResponse == "Successful Login") {
                 this.$router.push('welcome')
+              }
     }
 
     }
--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -2,11 +2,46 @@
     <div class="WelcomePage">
         <h1> Welcome </h1>
         <p> You're in! </p>
+                 <div class="my-2">
+            <v-btn v-on:click="logOff" small color="primary" dark>Log Off</v-btn>
+
+
+          </div>
+                                         <span align="center">
+     <b> {{ apiResponse }} </b>
+  </span>
+  <br>
     </div>
+
 </template>
 
 <script>
 export default {
-        name: 'WelcomePage'
-}
+    data(){
+        return {
+        name: 'WelcomePage',
+        apiResponse: null,
+        }
+    },
+    methods: {
+          
+            async logOff() {
+                
+              let apiResponse = '';
+
+                fetch("http://localhost:5094/user/loggOff", {
+                method: 'POST',
+                headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+              }).then(res => res.text())
+              .then(text => apiResponse = text)
+
+              this.apiResponse = apiResponse;
+              console.log(apiResponse);
+
+                this.$router.push('/')
+              
+    }
+
+    }
+    }
 </script>
